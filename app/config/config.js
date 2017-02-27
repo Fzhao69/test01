cead94458c796c5209cd20820ebb467bcffb28b8
wangyf <wangyf@cn-etc.com>
2017-02-27 14:00:44 +0800

    peizhi

--- app/config/config.js
+++ app/config/config.js
@@ -1,17 +1,16 @@
 module.exports = {
 	DB_HOST:"192.168.1.97",
 	DB_PORT: "30152",
-	DB_NAME: "252f3a87cea94",
+	DB_NAME: "4977b54bb7584",
 	DB_USERNAME: "4977b54bb7584",
 	DB_PASSWORD: "2833ddb78e134",
-	SESSION_KEY:"warehouse",
+	SESSION_KEY:"warehouse_",
 	SESSION_TABLE:"admin_sessions",
 
 	getConnectionUri:function(){
 		var protocol = "mongodb://";
-		var account = this.DB_USERNAME != "" ? this.DB_USERNAME + ":" + this.DB_PASSWORD + "@" : "";
+		var account = this.DB_USERNAME != "" ? this.DB_USERNAME + ":" + this.DB_PASSWORD  + "@" : "";
 		var port = this.DB_PORT === "27017" ? "" : ":" + this.DB_PORT;
 		return (protocol + account + this.DB_HOST + port + "/" + this.DB_NAME);
 	}
-	
 }
