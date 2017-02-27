37b9539693bec68acf710983447363d1e759c4fa
wangyf <wangyf@cn-etc.com>
2017-02-27 13:38:58 +0800

    数据库配更改

--- app/config/config.js
+++ app/config/config.js
@@ -1,16 +1,16 @@
-module.exports ={
+module.exports = {
 	DB_HOST:"192.168.1.97",
-	DB_PORT: "30139",
-	DB_NAME: "a9ecb1d6d9664",
-	DB_USERNAME: "1104a761af3b4",
-	DB_PASSWORD: "ee83f95bbe524",
+	DB_PORT: "30152",
+	DB_NAME: "252f3a87cea94",
+	DB_USERNAME: "4977b54bb7584",
+	DB_PASSWORD: "2833ddb78e134",
 	SESSION_KEY:"warehouse",
 	SESSION_TABLE:"admin_sessions",
 
 	getConnectionUri:function(){
 		var protocol = "mongodb://";
-		var account = this.DB_USERNAME != "" ? this.DB_USERNAME + ":" + this.DB_PASSWORD  + "@" : "";
-		var port = this.DB_PORT === "27017" ? "": ":" + this.DB_PORT ;
+		var account = this.DB_USERNAME != "" ? this.DB_USERNAME + ":" + this.DB_PASSWORD + "@" : "";
+		var port = this.DB_PORT === "27017" ? "" : ":" + this.DB_PORT;
 		return (protocol + account + this.DB_HOST + port + "/" + this.DB_NAME);
 	}
 }
